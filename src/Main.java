96153c4f79ba2f153f1a6164b6d56ec11f6aed03
SophiePaillocher <sophie.paillocher@hotmail.fr>
2019-02-13 14:58:47 +0100

    Questions 1 et 2

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class Main {
+    public static void main( String[] args )
+    {
+        InetAddress addr;
+        int port;
+
+        try {
+            if (args.length == 3) {
+                addr = InetAddress.getByName(args[1]);
+                port = Integer.parseInt(args[2]);
+            } else {
+                System.err.println("serveur et/ou port manquant");
+                return;
+            }
+
+            //byte[] buffer = new byte[100];
+            DatagramSocket ds = new DatagramSocket();
+
+            Scanner input = new Scanner(System.in);
+            String line;
+            do {
+                line = input.nextLine() + "\n";
+                byte[] line_b = line.getBytes();
+                DatagramPacket packet = new DatagramPacket(line_b, line_b.length, addr, port);
+                ds.send(packet);
+            } while (input.hasNextLine());
+
+            ds.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
